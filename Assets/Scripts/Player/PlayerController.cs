9c990fc255cf32ec9c2ec2cea957e9d1359410a3
AlexG <pg26alex@vfs.com>
2024-08-26 18:50:19 -0700

    Added prefab

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,9 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField, Tooltip("How farfg can the player move left and right")]
+    private float HopIncrement = 25f;
+
+    private Rigidbody rb;
+
+    private void OnValidate()
+    {
+        // Add
+        if (!rb)
+        {
+            if (!(rb = GetComponent<Rigidbody>()))
+            {
+                rb = gameObject.AddComponent<Rigidbody>();
+            }
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
