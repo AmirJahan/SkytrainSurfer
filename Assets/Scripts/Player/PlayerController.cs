30a3cd50a281b4e8a22ce1f897928a6296c0fcbb
AlexG <pg26alex@vfs.com>
2024-08-26 19:38:23 -0700

    Fully implmented jump

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,12 +5,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Hop Settings")]
     [SerializeField, Tooltip("How farfg can the player move left and right")]
     private float hopIncrement = 25f;
 
     [SerializeField, Tooltip("How long the player takes to hop left or right")]
     private float hopSpeed = 2f;
 
+    
+    [Header("Jump Settings")]
+    [SerializeField, Tooltip("How many player heights the player can jump")]
+    float jumpHeight = 2f;
+    
+    [SerializeField, Tooltip("How long the player takes to jump")]
+    float jumpSpeed = 2f;
+    
+    [SerializeField, Tooltip("How long the player can stay in the air")]
+    float jumpHangTime = 0.5f;
+    
+    
+    [Header("Controller Values")]
     [SerializeField, Tooltip("The lane the player is currently in")]
     private int lane = 0;
     
@@ -22,6 +36,8 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody rb;
 
+    
+    
     private void OnValidate()
     {
         // Add
@@ -34,12 +50,16 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    
+    
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    
+    
     private void Update()
     {
         if (isAlive)
@@ -61,6 +81,13 @@ public class PlayerController : MonoBehaviour
             {
                 StartCoroutine(HopToSide(1));
             }
+            
+            else if (Input.GetKeyDown(KeyCode.Space))
+            {
+                StartCoroutine(Jump());
+            }
+            
+            
         }
     }
     
@@ -79,18 +106,43 @@ public class PlayerController : MonoBehaviour
         float targetX = transform.position.x + (hopIncrement * direction);
         pauseInput = true;
         
+        // The destination of the player
         Vector3 dest = new Vector3(targetX, transform.position.y, transform.position.z);
         
         // Lerp the palyer to it's new position
         while (Vector3.Distance(transform.position, dest) > 0.1f)
         {
-
+            // move the player the next step
             float newPosition = Mathf.Lerp(transform.position.x, targetX, hopSpeed);
             rb.MovePosition(new Vector3(newPosition, transform.position.y, transform.position.z));
 
             yield return new WaitForFixedUpdate();
         }
         
+        pauseInput = false;
+    }
+
+
+    
+    IEnumerator Jump()
+    {
+        pauseInput = true;
+        
+        MeshRenderer Mesh = GetComponent<MeshRenderer>();
+        float targetJumpLocation = transform.position.y + (jumpHeight * Mesh.bounds.size.y);
+
+        // the jump destination world point
+        Vector3 dest = new Vector3(transform.position.x, targetJumpLocation, transform.position.z);
+        
+        while (Vector3.Distance(transform.position, dest) > 0.1f)
+        {
+            yield return new WaitForFixedUpdate();
+            
+            // Move the player over to the next step
+            float newPosition = Mathf.Lerp(transform.position.y, targetJumpLocation, jumpSpeed);
+            rb.MovePosition(new Vector3(transform.position.x, newPosition, transform.position.z));
+        }
+
         pauseInput = false;
     }
 }
