e074619b3b9ee3e406c12576be4b48205581a1af
Levi <pg26levi@vfs.com>
2024-08-30 19:51:54 -0700

    Added more generation

--- Assets/LevelGen/LevelGenerator.cs
+++ Assets/LevelGen/LevelGenerator.cs
@@ -51,13 +51,15 @@ public class LevelGenerator : MonoBehaviour
     // Responsible for hashing and enlisting blocks into blockchain
     void PopulateBlockchain(ChunkScript Chunk)
     {
-        Sector test = (Sector)Random.Range(0, 3);
-        test = 0;
-        switch (test)
+        Sector randomSector = (Sector)Random.Range(0, 3);
+
+        // TODO: Refactor ltr
+
+        switch (randomSector)
         {
             case Sector.SideTrains:
-                Chunk.AddObstacle(ObstacleType.Train, 0, 6 * 5);
-                Chunk.AddObstacle(ObstacleType.Train, 2, 6 * 8);
+                Chunk.AddObstacle(ObstacleType.Train, 0, 6 * Random.Range(5, 8));
+                Chunk.AddObstacle(ObstacleType.Train, 2, 6 * Random.Range(5, 8));
 
                 for (int i = 0; i < 16; i++)
                 {
@@ -69,8 +71,30 @@ public class LevelGenerator : MonoBehaviour
                 }
                 break;
             case Sector.LeftTrain:
+                Chunk.AddObstacle(ObstacleType.Train, 1, 6 * 5);
+                Chunk.AddObstacle(ObstacleType.Train, 2, 6 * 8);
+
+                for (int i = 0; i < 16; i++)
+                {
+                    bool shouldSpawn = Random.Range(0, 3) == 0;
+                    if (!shouldSpawn) continue;
+
+                    int obstacleType = Random.Range(1, 3);
+                    Chunk.AddObstacle((ObstacleType)obstacleType, 0, 6 * i);
+                }
                 break;
             case Sector .RightTrain:
+                Chunk.AddObstacle(ObstacleType.Train, 0, 6 * 5);
+                Chunk.AddObstacle(ObstacleType.Train, 1, 6 * 8);
+
+                for (int i = 0; i < 16; i++)
+                {
+                    bool shouldSpawn = Random.Range(0, 3) == 0;
+                    if (!shouldSpawn) continue;
+
+                    int obstacleType = Random.Range(1, 3);
+                    Chunk.AddObstacle((ObstacleType)obstacleType, 2, 6 * i);
+                }
                 break;
         }
 
