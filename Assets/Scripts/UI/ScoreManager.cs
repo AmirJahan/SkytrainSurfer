d8039b08a9dd759f17fe053b91b9f4757a4322b6
Xaviiour <158215530+pg26xav@users.noreply.github.com>
2024-09-09 19:17:42 -0700

    fixed paused issue

--- Assets/Scripts/UI/ScoreManager.cs
+++ Assets/Scripts/UI/ScoreManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -6,7 +7,34 @@ using UnityEngine;
 public class ScoreManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI highScoreText;
+    private int score;
+    private int highScore;
     private Coin coin;
-    
-    
+
+    private bool isHighScore;
+
+    private void Start()
+    {
+        if (PlayerPrefs.GetInt("HighScore") != null) highScore = PlayerPrefs.GetInt("HighScore");
+        
+    }
+
+    private void Update()
+    {
+        if (isHighScore) score = coin.coins;
+        if (score > highScore)
+        {
+            highScore = score;
+            PlayerPrefs.SetInt("HighScore", highScore);
+        }
+
+        UpdateText();
+    }
+
+    private void UpdateText()
+    {
+        scoreText.text = "Score: " + score;
+        highScoreText.text = "High Score: " + highScore;
+    }
 }
