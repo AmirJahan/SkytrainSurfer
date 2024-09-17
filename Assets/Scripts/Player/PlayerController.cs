075cf32d9bfbb3ecbeebca3780e77ac1b2716287
Alisa Ho <hckalisa@gmail.com>
2024-09-16 19:30:54 -0700

    Added sound

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     static PlayerController Instance;
-    
+
     public static PlayerController GetInstance()
     {
         if (!Instance)
@@ -16,8 +16,8 @@ public class PlayerController : MonoBehaviour
 
         return Instance;
     }
-    
-    
+
+
     [Header("Hop Settings")]
     [SerializeField, Tooltip("How farfg can the player move left and right")]
     private float hopIncrement = 25f;
@@ -25,59 +25,59 @@ public class PlayerController : MonoBehaviour
     [SerializeField, Tooltip("How long the player takes to hop left or right")]
     private float hopSpeed = 2f;
 
-    
+
     [Header("Jump Settings")]
     [SerializeField, Tooltip("How many player heights the player can jump")]
     float jumpHeight = 2f;
-    
+
     [SerializeField, Tooltip("How long the player takes to jump")]
     float jumpSpeed = 2f;
-    
+
     [SerializeField, Tooltip("How long the player can stay in the air")]
     float jumpHangTime = 0.5f;
-    
+
     [SerializeField, Tooltip("Whether or not the player has jumped")]
     bool hasJumped = false;
 
     private bool fastFall = false;
-    
+
     [SerializeField, Tooltip("The amount of health the currently player has")]
     float currentHealth = 100f;
-    
+
     [SerializeField, Tooltip("The amount of health the  player can have at most")]
     float maxtHealth = 100f;
-    
+
     [SerializeField, Tooltip("The amount of damage an obstacle does to the player")]
     float obstacleDamage = 10f;
 
 
     [Header("Slide settings")]
-    
+
     [SerializeField, Tooltip("Slide duration")]
     private float slideDuration = 0.5f;
 
     [SerializeField, Tooltip("Slide transition time")]
     private float slideTransitionTime = 0.25f;
-    
+
     [SerializeField, Tooltip("How fast the character falls when sliding in air")]
     float fastFallMultiplier = 150f;
-    
-    
+
+
     [Header("Controller Values")]
     [SerializeField, Tooltip("The lane the player is currently in")]
     private int lane = 0;
 
-    
+
     [SerializeField, Tooltip("Whether or not the player is currently in the middle of an action")]
     private bool pauseInput = false;
-    
+
     [SerializeField, Tooltip("Whether or not the player is alive")]
     bool isAlive = true;
 
     private Rigidbody rb;
     private PlayerInput input;
     private CapsuleCollider col;
-    
+
     private void OnValidate()
     {
         // Add
@@ -87,23 +87,23 @@ public class PlayerController : MonoBehaviour
             {
                 rb = gameObject.AddComponent<Rigidbody>();
             }
-            
+
             rb.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionX;
-            
+
         }
-        
+
         if (!input)
         {
             if (!(input = GetComponent<PlayerInput>()))
             {
                 input = gameObject.AddComponent<PlayerInput>();
             }
-            
+
         }
     }
 
-    
-    
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -112,8 +112,8 @@ public class PlayerController : MonoBehaviour
         currentHealth = maxtHealth;
     }
 
-    
-    
+
+
     private void Update()
     {
         if (isAlive)
@@ -122,15 +122,15 @@ public class PlayerController : MonoBehaviour
             if (!pauseInput)
             {
                 Vector2 moveDir = input.MoveDir;
-                
-                int xIn = Mathf.RoundToInt( moveDir.x );
+
+                int xIn = Mathf.RoundToInt(moveDir.x);
                 if (xIn != 0)
                 {
-                    StartCoroutine(HopToSide(xIn) );
+                    StartCoroutine(HopToSide(xIn));
                 }
-                
-                int yIn = Mathf.RoundToInt( moveDir.y );
-                
+
+                int yIn = Mathf.RoundToInt(moveDir.y);
+
                 if (yIn > 0)
                 {
                     StartCoroutine(Jump());
@@ -147,7 +147,7 @@ public class PlayerController : MonoBehaviour
 
         }
     }
-    
+
 
 
     // Causes the player to hop left or right
@@ -157,10 +157,12 @@ public class PlayerController : MonoBehaviour
         // Don't allow the player to hop if they are at the edge of the screen
         if ((lane == -1 && direction == -1) || (lane == 1 && direction == 1) || pauseInput)
             yield break;
-        
+
         pauseInput = true;
         lane += direction;
-        
+
+        AudioManager.Instance.PlaySFX("Slide");
+
         // The target hop position
         float targetX = transform.position.z + (hopIncrement * direction);
 
@@ -177,19 +179,19 @@ public class PlayerController : MonoBehaviour
                 startedY = transform.position.y;
                 dest = new Vector3(transform.position.x, transform.position.y, targetX);
             }
-            
+
             // move the player the next step
             float newPosition = Mathf.Lerp(transform.position.z, targetX, hopSpeed);
             rb.MovePosition(new Vector3(transform.position.x, transform.position.y, newPosition));
 
             yield return new WaitForFixedUpdate();
         }
-        
+
         pauseInput = false;
     }
 
 
-    
+
     IEnumerator Jump()
     {
         if (hasJumped)
@@ -200,8 +202,10 @@ public class PlayerController : MonoBehaviour
         hasJumped = true;
         fastFall = false;
         rb.useGravity = false;
-        
-        
+
+        AudioManager.Instance.PlaySFX("Jump");
+
+
         MeshRenderer Mesh = GetComponent<MeshRenderer>();
         float targetJumpLocation = transform.position.y + (jumpHeight * Mesh.bounds.size.y);
 
@@ -209,7 +213,7 @@ public class PlayerController : MonoBehaviour
         Vector3 dest = new Vector3(transform.position.x, targetJumpLocation, transform.position.z);
 
         int startedLayer = lane;
-        
+
         while (Vector3.Distance(transform.position, dest) > 0.1f)
         {
             if (fastFall)
@@ -224,7 +228,7 @@ public class PlayerController : MonoBehaviour
             }
 
             yield return new WaitForFixedUpdate();
-            
+
             // Move the player over to the next step
             float newPosition = Mathf.Lerp(transform.position.y, targetJumpLocation, jumpSpeed);
             rb.MovePosition(new Vector3(transform.position.x, newPosition, transform.position.z));
@@ -233,20 +237,21 @@ public class PlayerController : MonoBehaviour
         // how long to hang in air
         if (!fastFall)
             yield return new WaitForSeconds(jumpHangTime);
-        
+
         rb.useGravity = true;
     }
-    
-    
+
+
     IEnumerator Slide()
     {
+        AudioManager.Instance.PlaySFX("Slide");
         rb.AddForce(Vector3.down * fastFallMultiplier);
         fastFall = true;
-        
+
         Vector3 dest = new Vector3(transform.position.x, transform.position.y - (col.height / 4), transform.position.z);
-        
+
         int startedLayer = lane;
-        while ( Vector3.Distance( transform.position, dest) > 0.1f)
+        while (Vector3.Distance(transform.position, dest) > 0.1f)
         {
             if (lane != startedLayer)
             {
@@ -262,14 +267,14 @@ public class PlayerController : MonoBehaviour
         }
 
         col.height /= 4;
-        
+
         yield return new WaitForSeconds(slideDuration);
-        
+
         col.height *= 4;
-        
+
         dest = new Vector3(transform.position.x, transform.position.y + (col.height / 4), transform.position.z);
         startedLayer = lane;
-        while ( Vector3.Distance( transform.position, dest) > 0.1f )
+        while (Vector3.Distance(transform.position, dest) > 0.1f)
         {
             if (lane != startedLayer)
             {
@@ -279,10 +284,10 @@ public class PlayerController : MonoBehaviour
             {
                 break;
             }
-            
+
             transform.position = Vector3.Lerp(transform.position, dest, slideTransitionTime * Time.deltaTime);
         }
-        
+
         if (!hasJumped)
             rb.AddForce(Vector3.up);
 
@@ -294,22 +299,22 @@ public class PlayerController : MonoBehaviour
         hasJumped = false;
         pauseInput = false;
         fastFall = false;
-        
+
         if (other.gameObject.CompareTag("Obstacle"))
         {
             // destrpy tje obstacle
             Destroy(other.gameObject);
-            
+
             // reduce the player's health
             currentHealth -= obstacleDamage;
-            
+
             // check if the player is dead
             if (currentHealth <= 0)
             {
                 isAlive = false;
             }
         }
-        
+
     }
 
 
