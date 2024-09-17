27f34050f18ecd3dcf9facaa514dc445295bc50a
Gonk Droid <pg26spencer@vfs.com>
2024-09-16 19:03:07 -0700

    animations

--- Assets/AnimationInputs.cs
+++ Assets/AnimationInputs.cs
@@ -5,54 +5,43 @@ using UnityEngine;
 
 public class AnimationInputs : MonoBehaviour
 {
-    private bool isGrounded = true;
+    public enum ActionType
+    {
+        Jump,
+        SideJump,
+        Roll,
+        Victory
+    }
     
-    void Update()
+    public void PlayAction(ActionType action)
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
+        switch (action)
         {
-            GetComponentInChildren<Animator>().SetTrigger("Jump");
-            GetComponent<Rigidbody>().AddForce(Vector3.up * 300);
-        }
-
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && isGrounded)
-        {
-            GetComponentInChildren<Animator>().SetTrigger("SideJump");
-            GetComponent<Rigidbody>().AddForce(Vector3.up * 200);
-            GetComponent<Rigidbody>().AddForce(Vector3.left * 50);
-        }
-
-        if (Input.GetKeyDown(KeyCode.RightArrow) && isGrounded)
-        {
-            GetComponentInChildren<Animator>().SetTrigger("SideJump");
-            GetComponent<Rigidbody>().AddForce(Vector3.up * 200);
-            GetComponent<Rigidbody>().AddForce(Vector3.right * 50);
-        }
-
-        if (Input.GetKeyDown(KeyCode.DownArrow) && isGrounded)
-        {
-            GetComponentInChildren<Animator>().SetTrigger("Roll");
-        }
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            GetComponentInChildren<Animator>().SetBool("Victory", true);
-        }
-        else
-        {
-            GetComponentInChildren<Animator>().SetBool("Victory", false);
+            case (ActionType.Jump):
+            {
+                GetComponentInChildren<Animator>().SetTrigger("Jump");
+                break;
+            }
+            case (ActionType.SideJump):
+            {
+                GetComponentInChildren<Animator>().SetTrigger("SideJump");
+                break;
+            }
+            case (ActionType.Roll):
+            {
+                GetComponentInChildren<Animator>().SetTrigger("Roll");
+                break;
+            }
+            case (ActionType.Victory):
+            {
+                GetComponentInChildren<Animator>().SetBool("Victory", true);
+                break;
+            }
         }
     }
 
-    private void OnCollisionEnter(Collision other)
+    public void UpdateGrounded(bool isGrounded)
     {
-        GetComponentInChildren<Animator>().SetBool("Grounded", true);
-        isGrounded = true;
-    }
-
-    private void OnCollisionExit(Collision other)
-    {
-        GetComponentInChildren<Animator>().SetBool("Grounded", false);
-        isGrounded = false;
+        GetComponentInChildren<Animator>().SetBool("Grounded", isGrounded);
     }
 }
