60469a5fc9e4243f59e1059d974929be38885a48
Xaviiour <158215530+pg26xav@users.noreply.github.com>
2024-10-07 17:31:07 -0700

    score stuff

--- Assets/Scripts/UI/Score.cs
+++ Assets/Scripts/UI/Score.cs
@@ -6,22 +6,9 @@ using UnityEngine;
 public class Score : ScoreManager
 {
     [SerializeField] private TextMeshProUGUI scoreText;
-    [SerializeField] private TextMeshProUGUI highScoreText;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (PlayerPrefs.GetInt("HighScore") != null) highScore = PlayerPrefs.GetInt("HighScore");
-    }
 
     private void Update()
     {
-        UpdateText();
-    }
-
-    private void UpdateText()
-    {
         scoreText.text = "Score: " + score;
-        highScoreText.text = "High Score: " + highScore;
     }
 }
--- Assets/Scripts/UI/ScoreManager.cs
+++ Assets/Scripts/UI/ScoreManager.cs
@@ -1,28 +1,11 @@
-using TMPro;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
     protected int score;
-    protected int highScore;
-
-    protected bool isHighScore;
 
     private void Awake()
     {
-        //highScore = 0;
         score = DistanceScore.instance.dist;
-        if (PlayerPrefs.GetInt("HighScore") == null) highScore = PlayerPrefs.GetInt("HighScore");
-    }
-
-    private void Update()
-    {
-        if (isHighScore) highScore = DistanceScore.instance.dist;
-        if (score > highScore)
-        {
-            Debug.Log("huh");
-            highScore = score;
-            PlayerPrefs.SetInt("HighScore", highScore);
-        }
     }
 }
